95acaffc17ed8d5fe7b5573c9d602decf914dde1
chao.diangen <chao.diangen@trs.com.cn>
2022-09-15 17:57:37 +0800

    '搜索'

--- App.vue
+++ App.vue
@@ -16,7 +16,7 @@
 	/*每个页面公共css */
 	@import '@/common/uni.css';
 	@import '@/common/common.css';
-	@import url('https://at.alicdn.com/t/c/font_2888072_6hnbyrwv038.css');
+	@import url('https://at.alicdn.com/t/c/font_2888072_8z4k0fbw1kh.css');
 	::-webkit-scrollbar {
 	    display: none;
 	    width: 0 !important;
