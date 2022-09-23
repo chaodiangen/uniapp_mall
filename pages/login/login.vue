9d6b176663b0bab29106996873f4c7779f465d7f
chao.diangen <chao.diangen@trs.com.cn>
2022-09-23 16:00:31 +0800

    '增加'

---
+++ pages/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="login">
+		<view class="login-user">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
